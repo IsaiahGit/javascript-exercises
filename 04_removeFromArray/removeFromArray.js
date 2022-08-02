6da1a70452ca943d19849511c71839a705b14202
Isaiah Elder <isaiahcelder@gmail.com>
2022-08-02 13:06:33 -0700

    Finished 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+    let result = [];
+    let flag = true;
+    for (let j = 0; j < arr.length; j++) {
+        flag = true;
+        for (let i = 0; i < args.length; i++)
+            if (arr[j] === args[i]) {
+                flag = false;
+            }
+        if (flag) {
+            result.push(arr[j]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number")
+        return "ERROR";
+    if (num1 > num2) {
+        let temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    num1 -= 1;
+    num1 = ((num1 * num1 + num1) / 2);
+    num2 = ((num2 * num2 + num2) / 2);
+    return num2 - num1;
 };
 
 // Do not edit below this line
